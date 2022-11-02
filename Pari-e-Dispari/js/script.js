4d94eaa2b13099ace2ff5db96de6561fe6515dcc
Luigi9411 <luigi.sciavicco@gmail.com>
2022-11-02 20:38:59 +0100

    add-repository-pari-dispari

--- Pari-e-Dispari/js/script.js
+++ Pari-e-Dispari/js/script.js
@@ -1,22 +1,38 @@
-let divisibility = prompt ('Scegli pari o dispari').toLowerCase;
+let divisibility = prompt ('Scegli pari o dispari').toLowerCase();
 let userNumber = parseInt(prompt('Scegli un numero da 1 a 5'));
 let randomPc = randomNumber (1,5);
 let sum = userNumber + randomPc;
+let evenSum = evenSume(sum);
 console.log(sum);
-const eleResult = document.querySelector('.result')
-eleResult.innerHTML = winner();
+let eleResult = document.querySelector('.result')
+
+
+if (isDivisible(sum,divisibility)) {
+  eleResult.innerHTML = 'Hai vinto';
+} else {
+  eleResult.innerHTML = 'Ha vinto il pc';
+}
+
 
 
 function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
 
-function winner (){
-    if ((sum % 2 == 0) && (divisibility == 'pari')){
-      Result= 'Hai vinto';
-      return  Result;
-    } else if (((sum % 2 != 0) && (divisibility == 'dispari'))) {
-      Result= 'Vince il pc';
-      return  Result;
-    }
-}  
+ function evenSume (sum){
+ if (sum % 2 == 0){
+   sum = 'pari';
+   console.log('pari');
+ } else {
+  sum = 'dispari';
+   console.log('dispari');
+ }
+ }
+
+
+function isDivisible(number,divisibility){
+  if ((divisibility === 'pari' && number % 2 === 0) || (divisibility === 'dispari' && number % 2 !== 0)){
+    return true;
+  }
+    return false;
+}
