d85ec6d7e6de2528dd525bd8a919f7b3cfe4daa6
Luigi9411 <luigi.sciavicco@gmail.com>
2022-11-02 17:57:27 +0100

    prime correzioni

--- Pari-e-Dispari/js/script.js
+++ Pari-e-Dispari/js/script.js
@@ -1,7 +1,8 @@
-const divisibility = prompt ('Scegli pari o dispari').toLowerCase;
-const userNumber = parseInt(prompt('Scegli un numero da 1 a 5'));
+let divisibility = prompt ('Scegli pari o dispari').toLowerCase;
+let userNumber = parseInt(prompt('Scegli un numero da 1 a 5'));
 let randomPc = randomNumber (1,5);
 let sum = userNumber + randomPc;
+console.log(sum);
 const eleResult = document.querySelector('.result')
 eleResult.innerHTML = winner();
 
@@ -11,17 +12,11 @@ function randomNumber(min, max) {
   }
 
 function winner (){
-    if ((sum % 2 == 0) && (divisibility.length = 'pari')){
+    if ((sum % 2 === 0) && (divisibility === 'pari')){
       Result= 'Hai vinto';
-    } else if ((sum % 2 != 0) && (divisibility.length = 'pari')) {
+      return  Result;
+    } else if (((sum % 2 !== 0) && (divisibility === 'dispari'))) {
       Result= 'Vince il pc';
-    }else if ((sum % 2 == 0) && (divisibility.length = 'dispari')) {
-      Result= 'Vince il pc';
-    }else if ((sum % 2 != 0) && (divisibility.length = 'dispari')) {
-      Result= 'Hai vinto';
+      return  Result;
     }
-    return  Result;
 }  
-
-
-
